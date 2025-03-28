e87ef0a1e1c6612172875f1aa8aefddcbb2e32fb
Kristina Maximova <maximovaki@gmail.com>
2025-03-28 22:31:55 +0300

    add db_manager.py

---
+++ src/db_manager.py
@@ -0,0 +1,110 @@
+from config import config
+import psycopg2
+
+
+class DBManager():
+    """ Класс для работы с таблицами в базе данных postgres"""
+
+    def __init__(self, database_name: str, params: dict, ):
+        """ Конструктор класса для работы с таблицами в базе данных postgres """
+        self.conn = psycopg2.connect(dbname=database_name, **params)
+        self.cur = self.conn.cursor()
+
+    def get_companies_and_vacancies_count(self) -> list[dict] | None:
+        """ Метод для получения списка всех компаний
+         и количества вакансий у каждой компании"""
+        try:
+            with self.conn:
+                with self.cur as cur:
+                    cur.execute("""
+                    SELECT DISTINCT e.name, COUNT(v.*) as vacancies_count
+                    FROM employers e
+                    FULL JOIN vacancies v USING(employer_id)
+                    GROUP BY e.employer_id
+                    ORDER BY vacancies_count DESC;
+                    """)
+                    data = self.cur.fetchall()   # [('X5 Digital', 20), ('ФГБУ ЦСП ФМБА России', 20),..., ('Trafficsoft', 3)]
+                    data_in_dict = [{"employer":d[0], "vacancies_count":d[1]} for d in data]
+                    return data_in_dict
+        finally:
+            self.conn.close()
+
+    def get_all_vacancies(self) -> list[dict] | None:
+        """ Метод для получения списка всех вакансий """
+        try:
+            with self.conn:
+                with self.cur as cur:
+                    cur.execute("""
+                    SELECT e.name, v.name, v.salary, v.url
+                    FROM vacancies v
+                    FULL JOIN employers e USING(employer_id);
+                    """)
+                    data = self.cur.fetchall()
+                    vacancies_in_dict = [{"employer" : d[0],
+                                     "name" : d[1],
+                                     "salary" : d[2],
+                                     "url" : d[3]}
+                                     for d in data]
+                    return vacancies_in_dict
+        finally:
+            self.conn.close()
+
+    def get_avg_salary(self) -> dict | None:
+        """ Метод для получения средней зарплаты по вакансиям"""
+        try:
+            with self.conn:
+                with self.cur as cur:
+                    cur.execute("""
+                    SELECT AVG(v.salary)::real as average_salary
+                    FROM vacancies v 
+                    WHERE v.salary IS NOT NULL;
+                    """)
+                    data = self.cur.fetchall()
+                    return {"avg": data[0][0]}
+        finally:
+            self.conn.close()
+
+    def get_vacancies_with_higher_salary(self) -> list[dict] | None:
+        """ Метод для получения списка вакансий,
+        у которых зарплата выше среднего значения по всем вакансиям"""
+        try:
+            with self.conn:
+                with self.cur as cur:
+                    cur.execute("""
+                    SELECT v.employer, v.name, v.salary, v.url
+                    FROM vacancies v
+                    WHERE  v.salary > (SELECT AVG(v.salary)::real FROM vacancies v 
+                    WHERE v.salary IS NOT NULL)
+                    ORDER BY v.salary DESC;
+                    """)
+                    data = self.cur.fetchall()
+                    vacancies_in_dict = [{"employer": d[0],
+                                          "name": d[1],
+                                          "salary": d[2],
+                                          "url": d[3]}
+                                         for d in data]
+                    return vacancies_in_dict
+        finally:
+            self.conn.close()
+
+
+    def get_vacancies_with_keyword(self, keyword: str) -> list[dict] | None:
+        """ Метод для получения вакансий
+        по ключевому слову в названии """
+        try:
+            with self.conn:
+                with self.cur as cur:
+                    cur.execute(f"""
+                    SELECT v.employer, v.name, v.salary, v.url
+                    FROM vacancies v
+                    WHERE v.name LIKE '%{keyword}%';
+                    """)
+                    data = self.cur.fetchall()
+                    vacancies_in_dict = [{"employer": d[0],
+                                          "name": d[1],
+                                          "salary": d[2],
+                                          "url": d[3]}
+                                         for d in data]
+                    return vacancies_in_dict
+        finally:
+            self.conn.close()
--- src/main.py
+++ src/main.py
@@ -3,9 +3,7 @@ from src.api_guide import get_employer_data, get_vacancies
 from src.creating_tables import create_database
 from src.save_data_to_tables import (save_employers_data,
                                      save_vacancies_data)
-
-
-
+from src.db_manager import DBManager
 
 
 
@@ -32,3 +30,23 @@ if __name__ == "__main__":
     create_database("hh_vacancies", params)
     save_employers_data(emp1_list, database_name, params)
     save_vacancies_data(vacs_list,database_name, params)
+
+    # Выполнение запросов
+    db_manager1 = DBManager(database_name, params)
+    # query1 = db_manager1.get_companies_and_vacancies_count()
+    # for elem in query1:
+    #     print(f"Компания {elem['employer']}, вакансий: {elem['vacancies_count']}")
+
+    # query2 = db_manager1.get_all_vacancies()
+    # print(query2[0:3])
+
+    # query3 = db_manager1.get_avg_salary()
+    # print(f"Cредняя зарплата: {query3["avg"]} руб.")
+
+    # query4 = db_manager1.get_vacancies_with_higher_salary()
+    # for vac in query4[0:3]:
+    #     print(vac)
+
+    query5 = db_manager1.get_vacancies_with_keyword("Python")
+    for vac in query5[0:3]:
+        print(vac)
